4056654bf85d113d4c483c196d26704360c49b11
i5heu <i5heu@i5h.eu>
2017-09-12 14:13:24 +0200

    devdevDEEEEEEEV

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package gosocial
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var db *sql.DB
@@ -15,9 +17,41 @@ func Init(dbTMP *sql.DB) {
 
 }
 
-func ApiHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HALLO")
-	fmt.Println("HALLO - A")
+type API2STRUCT struct {
+	PWD    string
+	APP    string
+	ID     int
+	Title1 string
+	Text1  string
+}
+
+func ApiHandler(w http.ResponseWriter, r *http.Request, AdminHASH string) { //THIS ONE IS WORKING WITH jsondataRequests
+	startAPI2 := time.Now()
+
+	APILogin := false
+
+	decoder := json.NewDecoder(r.Body)
+	var jsondata API2STRUCT
+	errSearch := decoder.Decode(&jsondata)
+	if errSearch != nil {
+		fmt.Fprintf(w, `{"Status":"ERROR"}`)
+		fmt.Println(errSearch)
+		checkErr(errSearch)
+		return
+	}
+
+	switch jsondata.APP {
+	case "WriteComment":
+		WriteComment(w, jsondata)
+	default:
+		fmt.Fprintf(w, `{"Status":"ERROR - NO METHOD"}`)
+	}
+
+	fmt.Println("Api2Handler:", time.Since(startAPI2), APILogin)
+}
+
+func WriteComment(w http.ResponseWriter, jsondata API2STRUCT) {
+	fmt.Println("Api2Handler")
 }
 
 func checkErr(err error) {
