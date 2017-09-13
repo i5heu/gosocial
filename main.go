d729f25c8f9da383e50e7c2a98e42f63d8379762
i5heu <i5heu@i5h.eu>
2017-09-13 15:17:01 +0200

    Bring it to Alpha

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
 )
 
 var db *sql.DB
@@ -22,23 +23,25 @@ func Init(dbTMP *sql.DB) {
 }
 
 type API2STRUCT struct {
-	PWD   string
-	APP   string
-	ID    int
-	Slug  string
-	Name  string
-	Title string
-	Text  string
+	PWD         string
+	APP         string
+	ID          int
+	Slug        string
+	Name        string
+	Title       string
+	Text        string
+	ModerateNum int
 }
 
 type GetCommentsResults struct {
-	Id        int
-	Name      template.HTML
-	Title     template.HTML
-	Text      template.HTML
-	Upvotes   int
-	Downvotes int
-	Color     string
+	Id         int
+	Slug       template.HTML
+	Name       template.HTML
+	Title      template.HTML
+	Text       template.HTML
+	Upvotes    int
+	Downvotes  int
+	ModRelease int
 }
 
 type GetCommentsResultsArray struct { //GetCommentsResults Array
@@ -70,14 +73,30 @@ func ApiHandler(w http.ResponseWriter, r *http.Request, AdminHASH string) { //TH
 
 	switch jsondata.APP {
 	case "GetComments":
-		GetComments(w, r, jsondata)
+		GetComments(w, r, jsondata, AdminHASH)
 	case "WriteComment":
 		WriteComment(w, jsondata)
+	case "ModerateComment":
+		ModerateComment(w, r, jsondata, AdminHASH)
 	default:
 		fmt.Fprintf(w, `{"Status":"ERROR - NO METHOD"}`)
 	}
 
 	fmt.Println("Api2Handler:", time.Since(startAPI2))
+}
+
+func ModerateComment(w http.ResponseWriter, r *http.Request, jsondata API2STRUCT, AdminHASH string) {
+
+	if jsondata.PWD != AdminHASH {
+		fmt.Fprintf(w, `{"Status": "NOT LOGED IN"}`)
+		fmt.Println("Api2Handler-ModerateComment-NOTLOGEDIN")
+		return
+	}
+	fmt.Println(jsondata.ModerateNum, jsondata.ID)
+	db.Exec("UPDATE `gosocial_comments` SET `ModRelease` = ? WHERE `gosocial_comments`.`ID` = ?", jsondata.ModerateNum, jsondata.ID)
+
+	fmt.Fprintf(w, `{"Status": "OK"}`)
+	fmt.Println("Api2Handler-ModerateComment")
 }
 
 func WriteComment(w http.ResponseWriter, jsondata API2STRUCT) {
@@ -87,38 +106,38 @@ func WriteComment(w http.ResponseWriter, jsondata API2STRUCT) {
 	fmt.Println("Api2Handler-WriteComment")
 }
 
-func GetComments(w http.ResponseWriter, r *http.Request, jsondata API2STRUCT) {
+func GetComments(w http.ResponseWriter, r *http.Request, jsondata API2STRUCT, AdminHASH string) {
 
-	ids, err := db.Query("SELECT  ID, Name, Title, Text, upvotes, downvotes FROM gosocial_comments WHERE slug = ? AND ModRelease = '1' ORDER BY submitTime DESC LIMIT 1000", jsondata.Slug)
+	var ids *sql.Rows
+	var err error
+	if jsondata.PWD == AdminHASH {
+		ids, err = db.Query("SELECT  ID, slug ,Name, Title, Text, upvotes, downvotes, ModRelease FROM gosocial_comments WHERE ModRelease = 0 ORDER BY submitTime DESC LIMIT 1000")
+	} else {
+
+		ids, err = db.Query("SELECT  ID, slug, Name, Title, Text, upvotes, downvotes, ModRelease FROM gosocial_comments WHERE slug = ? AND ModRelease = 1 ORDER BY submitTime DESC LIMIT 1000", jsondata.Slug)
+	}
 	defer ids.Close()
 	checkErr(err)
 	var TMP []GetCommentsResults
-	var ClassSwitch bool = true
-	var ClassTMP = "ProjectTableDark"
 
 	for ids.Next() {
 		var id int
+		var slug string
 		var name string
 		var title string
 		var text string
 		var upvotes int
 		var downvotes int
-		_ = ids.Scan(&id, &name, &title, &text, &upvotes, &downvotes)
+		var ModRelease int
+		_ = ids.Scan(&id, &slug, &name, &title, &text, &upvotes, &downvotes, &ModRelease)
 		checkErr(err)
 
 		NameTMP := template.HTML(bluemonday.UGCPolicy().SanitizeBytes([]byte(name)))
 		TitleTMP := template.HTML(bluemonday.UGCPolicy().SanitizeBytes([]byte(title)))
-		TextTMP := template.HTML(bluemonday.UGCPolicy().SanitizeBytes([]byte(text)))
+		TextTMP := template.HTML(blackfriday.MarkdownCommon(bluemonday.UGCPolicy().SanitizeBytes([]byte(text))))
+		SlugTMP := template.HTML(bluemonday.UGCPolicy().SanitizeBytes([]byte(slug)))
 
-		if ClassSwitch == true {
-			ClassTMP = "ProjectTableDark"
-			ClassSwitch = false
-		} else {
-			ClassTMP = "ProjectTableBright"
-			ClassSwitch = true
-		}
-
-		TMP = append(TMP, GetCommentsResults{id, NameTMP, TitleTMP, TextTMP, upvotes, downvotes, ClassTMP})
+		TMP = append(TMP, GetCommentsResults{id, SlugTMP, NameTMP, TitleTMP, TextTMP, upvotes, downvotes, ModRelease})
 	}
 
 	lists := GetCommentsResultsArray{"OK", TMP}
